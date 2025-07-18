8fde2a2c86aadad85525d0053aec6fe3eebf2bdf
Alex Snegiref <97952380+xandrosanchez@users.noreply.github.com>
2025-07-18 16:54:21 +0300

    hw01-gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "otus_hw"
+include("hw01-gradle")
+
+pluginManagement {
+    val dependencyManagement: String by settings
+    val springframeworkBoot: String by settings
+    val johnrengelmanShadow: String by settings
+    val jib: String by settings
+    val protobufVer: String by settings
+
+    plugins {
+        id("io.spring.dependency-management") version dependencyManagement
+        id("org.springframework.boot") version springframeworkBoot
+        id("com.github.johnrengelman.shadow") version johnrengelmanShadow
+        id("com.google.cloud.tools.jib") version jib
+        id("com.google.protobuf") version protobufVer
+    }
+}
