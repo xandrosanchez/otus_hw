450b7f39d43cab99dd58981d634727b081444f19
xandrosanchez <din.myrk@gmail.com>
2025-10-09 01:05:42 +0300

    ДЗ №4 добавление программы

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "otus_hw"
 include("hw01-gradle")
 include("hw02-generics")
 include("hw03-annotation")
+include("hw04-gc")
 
 pluginManagement {
     val dependencyManagement: String by settings
